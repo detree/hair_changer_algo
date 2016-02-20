a1147f968f4c73cba025f4575a76f0de5021970f
Shuchen Song <ssong18@illinois.edu>
2016-02-20 13:18:40 -0600

    done with face range detection

---
+++ src/face_detect_test/Test.java
@@ -0,0 +1,16 @@
+package face_detect_test;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
+	    Mat mat = Mat.eye( 3, 3, CvType.CV_8UC1 );
+	    System.out.println( "mat = " + mat.dump() );
+	    new RawClassifier().face_range();
+	}
+
+}
